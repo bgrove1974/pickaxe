69ebcdc5596546346675f98e315f1e5e8bce5875
bgrove1974 <bgrove1974@gmail.com>
2014-12-08 01:22:54 -0800

    Palindromes

--- ch4/lazy_enumerator.rb
+++ ch4/lazy_enumerator.rb
@@ -15,3 +15,16 @@ end
 
 p Integer.all.first(10)
 # => [1, 2, 3, 4, 5, 6, 7 ,8, 9, 10]
+
+# Let's get the first 10 multiples of 3.
+
+p Integer
+        .all
+        .select { |i| (i % 3).zero? }
+        .first(10)
+# => [3, 6, 9, 12, 15, 18, 21, 24, 27, 30]
+
+# Without the lazy enumerator, the call to select would effectively never return,
+# as select would try to read all of the values from the generator.
+# In this case, the lazy version of select only consumes values on demand, and
+# the subsequent call to the 'first' method only asks for 10 values.
