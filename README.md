7ce6999e304cc34a87463028af7475a30e787174
bgrove1974 <bgrove1974@gmail.com>
2015-02-13 23:31:38 -0800

    Being a Good Packaging Citizen

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 Code from the pickaxe book, third edition.
 
-Ch 16.3 Being a Good Packaging Citizen pp 234
+Ch 16.3 Using RubyGems pp 234
---
+++ ch_16/16.3/good_packaging.rb
@@ -0,0 +1,8 @@
+# So, I have ignored some stuff that you would want to do before distributing your code to the world.
+# Your distributed directory tree really should have a README file, outlining what it does and probably containing a copyright statement; an INSTALL file, giving installation instructions; and a LICENSE file, giving the license that it is distributed under.
+
+# You will probably want to distribute some documentation as well.
+# This would go in a directory called doc/, parallel with the bin/ and lib/ directories.
+
+# You might also want to distribute native C-language extensions with your library.
+# These extensions would go in your project's ext/ directory.
